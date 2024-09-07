d690d084da1ce3e4131257a1c3d4d79f620f8c2f
VThanhDat <121113462+VThanhDat@users.noreply.github.com>
2024-09-07 21:50:59 +0700

    update

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -7,7 +7,6 @@ exports.create = async (req, res, next) => {
     if (!req.body?.name) {
         return next(new ApiError(400, "Name can not be empty"));
     }
-
     try {
         const contactService = new ContactService(MongoDB.client);
         const document = await contactService.create(req.body);
--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
 
 async function startServer() {
-    try {
+    try{
         await MongoDB.connect(config.db.uri);
         console.log("Connected to the database!");
 
         const PORT = config.app.port;
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on port ${PORT}.`);
         });
-    } catch (error) {
+    }catch(error){
         console.log("Cannot connect to the database!", error);
         process.exit();
     }
